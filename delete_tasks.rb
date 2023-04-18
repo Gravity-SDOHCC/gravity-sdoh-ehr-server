1305ddf9e523a90334e87cd57716918e4d86f3dc
Vanessa Fotso <vfotso@mitre.org>
2023-04-17 23:41:05 -0400

    ruby script to manage db

---
+++ delete_tasks.rb
@@ -0,0 +1,21 @@
+require "httparty"
+
+FHIR_SERVER = "http://localhost:8080/fhir"
+$count = 0
+$retry_resources = Array.new()
+
+# Fetch all task resources
+def fetch_tasks
+  response = HTTParty.get("#{FHIR_SERVER}/Task")
+  if response.code == 200
+    bundle = JSON.parse(response.body)
+    ids = bundle["entry"].map { |entry| entry["resource"] }.map { |resource| resource["id"] }
+    ids.each do |id|
+      resonse = HTTParty.delete("#{FHIR_SERVER}/Task/#{id}?_cascade=true")
+      puts resonse.code
+    end
+    # puts response.
+  end
+end
+
+fetch_tasks
