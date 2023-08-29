61d14bfd1f892195caec32840de97f12e5d5a7bc
Dylan Hall <dehall@mitre.org>
2023-08-29 11:42:00 -0400

    Migrate files from ./fhir_resources/ to src/main/resources/fhir_resources/

--- src/main/java/ca/uhn/fhir/jpa/starter/gravity/database/Loader.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/gravity/database/Loader.java
@@ -1,7 +1,11 @@
 package ca.uhn.fhir.jpa.starter.gravity.database;
 
 import java.io.File;
+import java.net.URI;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
@@ -17,49 +21,53 @@ import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import ca.uhn.fhir.parser.IParser;
 import ca.uhn.fhir.rest.api.server.SystemRequestDetails;
 
+/**
+ * The Loader class loads FHIR resource JSONs from file into the server upon startup.
+ * This is intended to be equivalent to HTTP PUT requests where the ID from the files is preserved.
+ */
 @Component
 public class Loader {
+  private static final org.slf4j.Logger LOG = org.slf4j.LoggerFactory.getLogger(Loader.class);
 
   @Autowired
   private ApplicationContext context;
 
   @EventListener(ApplicationReadyEvent.class)
-  public void loadResources() {
-    // TODO: will this work when packaged up? 
-    // may need to move these under src/main/resources
-    File dir = new File("./fhir_resources");
-
+  public void loadResources() throws Exception {
     Map<String, IFhirResourceDao> resourceDAOs = context.getBeansOfType(IFhirResourceDao.class);
-
     IParser parser = FhirContext.forR4().newJsonParser();
 
-    File[] files = dir.listFiles((d, name) -> name.endsWith(".json"));
-    if (files != null) {
-      for (File file : files) {
-        try {
-          System.out.println("Loading " + file.getName());
-          String fileContent = FileUtils.readFileToString(file, Charset.defaultCharset());
-          Resource resource = (Resource) parser.parseResource(fileContent);
-          
-          String resourceType = resource.getResourceType().toString();
-          
-          // IMPORTANT: the HAPI parser appends version numbers to the ID, but
-          // if you just read the JSON directly into the body of an HTTP PUT you probably don't.
-          // The version number causes pain here, so remove it.
-          resource.setId(resourceType + "/" + resource.getIdPart());
-         
-          // TODO: This feels brittle. May not be the right way to get a DAO.
-          // All FHIR resource DAOs appear to be named "my{resourceType}DaoR4"
-          IFhirResourceDao<Resource> resourceDAO = resourceDAOs.get("my" + resourceType + "DaoR4");          
-          resourceDAO.update(resource, new SystemRequestDetails());
-        } catch (Exception e) {
-          // TODO: logger
-          System.err.println("Unable to load " + file.getName());
-          e.printStackTrace();
-        }
-      }
-    }
+    URI resourcesURI = Loader.class.getClassLoader().getResource("fhir_resources").toURI();
+    Path fhirResources = Paths.get(resourcesURI);
 
-    System.out.println("Done loading files");
+    Files.walk(fhirResources, Integer.MAX_VALUE)
+        .filter(Files::isReadable)
+        .filter(Files::isRegularFile)
+        .filter(p -> p.toString().endsWith(".json"))
+        .forEach(p -> {
+          try {
+            File file = p.toFile();
+            LOG.info("Loading " + file.getName());
+            String fileContent = FileUtils.readFileToString(file, Charset.defaultCharset());
+            Resource resource = (Resource) parser.parseResource(fileContent);
+
+            String resourceType = resource.getResourceType().toString();
+
+            // IMPORTANT: the HAPI parser appends version numbers to this ID when parsing from file,
+            // but not when parsing from the body of an HTTP request, even when the content of both
+            // is exactly the same.
+            // That version number causes pain here, so remove it.
+            resource.setId(resourceType + "/" + resource.getIdPart());
+
+            // TODO: This feels brittle. May not be the right way to get a DAO.
+            // All FHIR resource DAOs appear to be named "my{resourceType}DaoR4"
+            IFhirResourceDao<Resource> resourceDAO = resourceDAOs.get("my" + resourceType + "DaoR4");          
+            resourceDAO.update(resource, new SystemRequestDetails());
+          } catch (Exception e) {
+            LOG.error("Unable to load " + p.toString(), e);
+          }
+        });
+
+    LOG.info("Done loading files");
   }
 }
